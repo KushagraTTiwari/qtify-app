cb566c28acaccd191831eb4c197215f55bfa85cf
Kushagra Tiwari <kushagratiawri6162@gmail.com>
2024-03-28 11:56:53 +0530

    first commit

---
+++ src/Navbar.js
@@ -0,0 +1,24 @@
+import logo from "./assets/logo.png";
+import search from "./assets/search-icon.png";
+import "./Navbar.css";
+import FeedBackcompo from "./feebback.js"
+
+
+
+const Navbar = () => {
+  const data = "Give Feedback"
+  return (
+    <div className="navbar">
+      <img src={logo} alt="logo" />
+      <div className="searchbar">
+        <input type="text" placeholder="Search a album of your choice" />
+        <button>
+          <img src={search} alt="icon"/>
+        </button>
+      </div>
+      <FeedBackcompo message={data}/>
+    </div>
+  );
+};
+
+export default Navbar;
---
+++ src/feebback.js
@@ -0,0 +1,8 @@
+import "./Navbar.css"
+const FeedBackcompo = (props) => {
+    return (
+        <button className="feed-btn">{props.message}</button>        
+    )
+}
+
+export default FeedBackcompo;
